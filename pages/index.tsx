33e124862c75ec123996d2db1a7e510eb079110f
Matt Greenfield <gmattgreenfield@gmail.com>
2023-01-12 15:58:49 +0000

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Cocktails</title>
+        <meta name="description" content="A cocktail list and finder" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={inter.className}>
+        {/*  */}
+      </main>
+    </>
+  );
+}
